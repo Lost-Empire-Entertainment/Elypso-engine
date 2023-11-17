2b6b9a1b0720fbcf6d2e56251645a785a83f3cc9
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-11-18 00:27:55 +0200

    cleanup

--- build.bat
+++ build.bat
@@ -1,3 +1,15 @@
-cmake .
-cpack
+REM Configure the project (Release build)
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+
+REM Build the project
+cmake --build build --config Release
+
+REM Install the project
+cmake --install build --config Release
+
+REM Additional logging for CMake configuration and CPack
+cmake . > cmake_log.txt 2>&1
+cpack > cpack_log.txt 2>&1
+
+REM Pause to keep the console window open for review
 pause
