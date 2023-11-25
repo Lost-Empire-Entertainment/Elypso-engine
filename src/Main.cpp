47589506cf647a30c4473c3b758f3b01a7869321
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-11-25 19:37:57 +0200

    green window

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -27,7 +28,7 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// glad: load all OpenGL function pointers
+	//check if glad is initialized before continuing
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -37,6 +38,12 @@ int main()
 	//keep window open until user closes it
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
+		//simple green color instead of black
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -46,6 +53,16 @@ int main()
 	return 0;
 }
 
+//process all input and react accordingly
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+//make sure the viewport matches the new window dimentions
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
