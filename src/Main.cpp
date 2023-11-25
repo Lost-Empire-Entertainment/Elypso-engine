d38a021b32aaf313f6e64cb59124abed9011b1cd
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-11-25 03:24:53 +0200

    app title name change

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,7 +16,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//create a window object holding all the windowing data
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Elypso engine", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
