90a60507548b11e36c5fb889b9ddb4aa224c27e7
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-12-07 19:36:46 +0200

    fixed ground pepe render

--- src/engine/graphics/render.cpp
+++ src/engine/graphics/render.cpp
@@ -164,17 +164,34 @@ namespace Graphics
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, Texture::textures[1]);
 
-		//create transformations
-		glm::mat4 transform = glm::mat4(1.0f);
-		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-
-		//get matrix uniform location and set matrix
+		//activate shader
 		shader->Use();
-		unsigned int transformLoc = glGetUniformLocation(shader->ID, "transform");
-		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-		//render crate
+		//create transformations
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		model = glm::rotate(
+			model, 
+			glm::radians(-55.0f), 
+			glm::vec3(1.0f, 0.0f, 0.0f));
+		view = glm::translate(
+			view, 
+			glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(
+			glm::radians(45.0f), 
+			(float)Render::SCR_WIDTH / (float)Render::SCR_HEIGHT, 
+			0.1f, 
+			100.0f);
+		//retrieve the matrix uniform locations
+		unsigned int modelLoc = glGetUniformLocation(shader->ID, "model");
+		unsigned int viewLoc = glGetUniformLocation(shader->ID, "view");
+		//pass them to the shaders
+		shader->SetMat4("model", model);
+		shader->SetMat4("view", view);
+		shader->SetMat4("projection", projection);
+
+		//render container
 		glBindVertexArray(Render::VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
