9c1bf684b8c9235e14b8dbac4debb01bcb25ce9e
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-12-20 01:00:53 +0200

    updated version number

--- include/engine/core/core.hpp
+++ include/engine/core/core.hpp
@@ -27,7 +27,7 @@ namespace Core
 	{
 	public:
 		static inline string name = "Elypso engine";
-		static inline string version = "0.0.8 Prototype";
+		static inline string version = "0.0.9 Prototype";
 		static inline string docsPath;
 		static inline string filesPath;
 
