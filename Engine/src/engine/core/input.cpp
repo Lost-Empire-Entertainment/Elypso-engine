2ad6c8b497a6d7d6cb5236611012ccf843391499
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-02-04 16:55:13 +0200

    fixed #23

--- Engine/src/engine/core/input.cpp
+++ Engine/src/engine/core/input.cpp
@@ -160,13 +160,13 @@ namespace Core
                     Render::camera.cameraPos += inputSettings.cameraSpeed * currentSpeed * right);
             }
             //camera up
-            if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+            if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
             {
                 Render::camera.SetCameraPosition(
                     Render::camera.cameraPos += inputSettings.cameraUp * inputSettings.cameraSpeed * currentSpeed);
             }
             //camera down
-            if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+            if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
             {
                 Render::camera.SetCameraPosition(
                     Render::camera.cameraPos -= inputSettings.cameraUp * inputSettings.cameraSpeed * currentSpeed);
