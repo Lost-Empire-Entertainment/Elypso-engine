27d9f5145750807cfd674e07272750b1472a434f
tkp <mileshenthorne1@gmail.com>
2024-11-17 21:44:00 -0500

    fixed sub-optimal code

--- Engine/src/engine/graphics/shapes/gameobject.cpp
+++ Engine/src/engine/graphics/shapes/gameobject.cpp
@@ -35,7 +35,6 @@ using std::cout;
 using std::endl;
 using std::sort;
 using std::to_string;
-using std::remove;
 using std::dynamic_pointer_cast;
 using glm::distance;
 using std::filesystem::directory_iterator;
@@ -152,7 +151,7 @@ namespace Graphics::Shape
 				GameObjectManager::DestroyGameObject(child);
 			}
 		}
-		//remove object from parent children vector
+		//std::remove object from parent children vector
 		if (obj->GetParent() != nullptr)
 		{
 			obj->GetParent()->RemoveChild(obj);
@@ -161,35 +160,35 @@ namespace Graphics::Shape
 		switch (type)
 		{
 		case Type::model:
-			objects.erase(remove(objects.begin(), objects.end(), obj), objects.end());
-			opaqueObjects.erase(remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
+			objects.erase(std::remove(objects.begin(), objects.end(), obj), objects.end());
+			opaqueObjects.erase(std::remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
 			break;
 		case Type::point_light:
 			DestroyGameObject(obj->GetChildBillboard());
 			obj->SetChildBillboard(nullptr);
-			objects.erase(remove(objects.begin(), objects.end(), obj), objects.end());
-			opaqueObjects.erase(remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
-			pointLights.erase(remove(pointLights.begin(), pointLights.end(), obj), pointLights.end());
+			objects.erase(std::remove(objects.begin(), objects.end(), obj), objects.end());
+			opaqueObjects.erase(std::remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
+			pointLights.erase(std::remove(pointLights.begin(), pointLights.end(), obj), pointLights.end());
 			break;
 		case Type::spot_light:
 			DestroyGameObject(obj->GetChildBillboard());
 			obj->SetChildBillboard(nullptr);
-			objects.erase(remove(objects.begin(), objects.end(), obj), objects.end());
-			opaqueObjects.erase(remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
-			spotLights.erase(remove(spotLights.begin(), spotLights.end(), obj), spotLights.end());
+			objects.erase(std::remove(objects.begin(), objects.end(), obj), objects.end());
+			opaqueObjects.erase(std::remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
+			spotLights.erase(std::remove(spotLights.begin(), spotLights.end(), obj), spotLights.end());
 			break;
 		case Type::directional_light:
 			DestroyGameObject(obj->GetChildBillboard());
 			obj->SetChildBillboard(nullptr);
-			objects.erase(remove(objects.begin(), objects.end(), obj), objects.end());
-			opaqueObjects.erase(remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
+			objects.erase(std::remove(objects.begin(), objects.end(), obj), objects.end());
+			opaqueObjects.erase(std::remove(opaqueObjects.begin(), opaqueObjects.end(), obj), opaqueObjects.end());
 			directionalLight = nullptr;
 			break;
 		case Type::billboard:
 			obj->SetParentBillboardHolder(nullptr);
-			objects.erase(remove(objects.begin(), objects.end(), obj), objects.end());
-			transparentObjects.erase(remove(transparentObjects.begin(), transparentObjects.end(), obj), transparentObjects.end());
-			billboards.erase(remove(billboards.begin(), billboards.end(), obj), billboards.end());
+			objects.erase(std::remove(objects.begin(), objects.end(), obj), objects.end());
+			transparentObjects.erase(std::remove(transparentObjects.begin(), transparentObjects.end(), obj), transparentObjects.end());
+			billboards.erase(std::remove(billboards.begin(), billboards.end(), obj), billboards.end());
 			break;
 		}
 
@@ -254,7 +253,7 @@ namespace Graphics::Shape
 				string key = splitLine[0];
 				string value = splitLine[1];
 
-				//remove one space in front of value if it exists
+				//std::remove one space in front of value if it exists
 				if (value[0] == ' ') value.erase(0, 1);
 
 				if (key == "name") name = value;
