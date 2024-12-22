96a319ea0e9ea92e286489721b1a76ebe87d5be9
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2024-12-22 18:43:50 +0200

    setting up components

---
+++ Engine/include/engine/graphics/components/meshcomponent.hpp
@@ -0,0 +1,116 @@
+//Copyright(C) 2024 Lost Empire Entertainment
+//This program comes with ABSOLUTELY NO WARRANTY.
+//This is free software, and you are welcome to redistribute it under certain conditions.
+//Read LICENSE.md for more information.
+
+#pragma once
+
+#include <vector>
+
+//external
+#include "glad.h"
+#include "gameobject.hpp"
+#include "glm.hpp"
+
+//engine
+#include "component.hpp"
+
+namespace Components
+{
+    using std::vector;
+    using glm::vec3;
+    using glm::vec2;
+
+    class MeshComponent : public Component
+    {
+    public:
+        struct AssimpVertex
+        {
+            vec3 pos{};
+            vec3 normal{};
+            vec2 texCoords{};
+            vec3 tangent{};
+            vec3 bitangent{};
+            int boneIDs = 4;
+            float weights = 4;
+        };
+
+        class AssimpMesh
+        {
+        public:
+            vector<AssimpVertex> vertices;
+            vector<unsigned int> indices;
+
+            AssimpMesh(
+                vector<AssimpVertex> vertices,
+                vector<unsigned int> indices) :
+                vertices(vertices),
+                indices(indices) {}
+        };
+
+        enum class MeshType
+        {
+            border,
+            actionTex,
+            model,
+            point_light,
+            spot_light,
+            directional_light,
+            billboard,
+            skybox
+        };
+
+        MeshComponent(
+            const bool& isEnabled,
+            const MeshType& type,
+            const GLuint& VAO,
+            const GLuint& VBO,
+            const GLuint& EBO) : 
+            isEnabled(isEnabled), 
+            type(type), 
+            VAO(VAO), 
+            VBO(VBO), 
+            EBO(EBO) {}
+
+        ~MeshComponent()
+        {
+            glDeleteVertexArrays(1, &VAO);
+            glDeleteBuffers(1, &VBO);
+            glDeleteBuffers(1, &EBO);
+        }
+
+        void AddAssimpMesh(const AssimpMesh& newMesh) { assimpMeshes.push_back(newMesh); }
+        const vector<AssimpMesh>& GetAssimpMeshes() const { return assimpMeshes; }
+
+        void SetEnableState(const bool& newIsEnabled) { isEnabled = newIsEnabled; }
+        void SetMeshType(const MeshType& newType) { type = newType; }
+        void SetVAO(const GLuint& newVAO) { VAO = newVAO; }
+        void SetVBO(const GLuint& newVBO) { VBO = newVBO; }
+        void SetEBO(const GLuint& newEBO) { EBO = newEBO; }
+        void SetVertices(const vector<AssimpVertex>& newVertices) { vertices = newVertices; }
+        void SetIndices(const vector<unsigned int>& newIndices) { indices = newIndices; }
+
+        const bool& IsEnabled() const { return isEnabled; }
+        const MeshType& GetMeshType() const { return type; }
+        const GLuint& GetVAO() const { return VAO; }
+        const GLuint& GetVBO() const { return VBO; }
+        const GLuint& GetEBO() const { return EBO; }
+        const vector<AssimpVertex>& GetVertices() const { return vertices; }
+        const vector<unsigned int>& GetIndices() const { return indices; }
+
+        type_index GetType() const override { return typeid(MeshComponent); }
+
+        void Update(float deltaTime) override {}
+
+    private:
+        bool isEnabled;
+        MeshType type;
+        GLuint VAO;
+        GLuint VBO;
+        GLuint EBO;
+        vector<AssimpVertex> vertices;
+        vector<unsigned int> indices;
+
+        vector<AssimpMesh> assimpMeshes;
+    };
+}
--- Engine/include/engine/graphics/shapes/gameobject.hpp
+++ Engine/include/engine/graphics/shapes/gameobject.hpp
@@ -10,6 +10,8 @@
 #include <map>
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <typeindex>
 
 //external
 #include "glad.h"
@@ -18,659 +20,155 @@
 //engine
 #include "shader.hpp"
 
+namespace Graphics::Components
+{
+	class Component;
+	class TransformComponent;
+}
+
 namespace Graphics::Shape
 {
 	using std::vector;
 	using std::map;
 	using std::shared_ptr;
+	using std::weak_ptr;
 	using std::make_shared;
 	using std::cout;
 	using glm::vec3;
 	using glm::mat4;
 	using std::string;
+	using std::type_index;
+	using std::enable_shared_from_this;
 
 	using Graphics::Shader;
+	using Graphics::Components::TransformComponent;
+	using Graphics::Components::Component;
 
-	class Transform
+	class GameObject : public enable_shared_from_this<GameObject>
 	{
 	public:
-		Transform(
-			const vec3& position,
-			const vec3& rotation,
-			const vec3& scale) :
-			position(position),
-			rotation(rotation),
-			scale(scale)
+		GameObject(const string& name, const string& txtFilePath)
+			: name(name),
+			ID(nextID++),
+			isInitialized(false),
+			isEnabled(true),
+			txtFilePath(txtFilePath),
+			transform(make_shared<TransformComponent>())
 		{
+			AddComponent<TransformComponent>(transform);
 		}
 
-		void SetPosition(const vec3& newPosition)
+		/*
+		*
+		* COMPONENTS
+		*
+		*/
+		template <typename T, typename... Args>
+		shared_ptr<T> AddComponent(Args&&... args)
 		{
-			position = newPosition;
+			auto component = make_shared<T>(forward<Args>(args)...);
+			components[typeid(T)] = component;
+			return component;
 		}
-		void SetRotation(const vec3& newRotation)
+		template <typename T>
+		shared_ptr<T> GetComponent() const
 		{
-			rotation = newRotation;
-		}
-		void SetScale(const vec3& newScale)
-		{
-			scale = newScale;
-		}
-
-		const vec3& GetPosition() const
-		{
-			return position;
-		}
-		const vec3& GetRotation() const
-		{
-			return rotation;
-		}
-		const vec3& GetScale() const
-		{
-			return scale;
-		}
-	private:
-		vec3 position;
-		vec3 rotation;
-		vec3 scale;
-	};
-
-	struct AssimpVertex
-	{
-		vec3 pos{};
-		vec3 normal{};
-		vec2 texCoords{};
-		vec3 tangent{};
-		vec3 bitangent{};
-		int boneIDs = 4;
-		float weights = 4;
-	};
-
-	class AssimpMesh
-	{
-	public:
-		vector<AssimpVertex> vertices;
-		vector<unsigned int> indices;
-
-		AssimpMesh(
-			vector<AssimpVertex> vertices,
-			vector<unsigned int> indices) :
-			vertices(vertices),
-			indices(indices)
-		{
-		}
-	};
-
-	class Mesh
-	{
-	public:
-		enum class MeshType
-		{
-			border,
-			actionTex,
-			model,
-			point_light,
-			spot_light,
-			directional_light,
-			billboard,
-			skybox
-		};
-
-		Mesh(const bool& isEnabled,
-			const MeshType& type,
-			const GLuint& VAO,
-			const GLuint& VBO,
-			const GLuint& EBO) :
-			isEnabled(isEnabled),
-			type(type),
-			VAO(VAO),
-			VBO(VBO),
-			EBO(EBO)
-		{
-		}
-		~Mesh()
-		{
-			glDeleteVertexArrays(1, &VAO);
-			glDeleteBuffers(1, &VBO);
-			glDeleteBuffers(1, &EBO);
-		}
-
-		void SetEnableState(const bool& newIsEnabled)
-		{
-			isEnabled = newIsEnabled;
-		}
-		void SetMeshType(const MeshType& newType)
-		{
-			type = newType;
-		}
-		void SetVAO(const GLuint& newVAO)
-		{
-			VAO = newVAO;
-		}
-		void SetVBO(const GLuint& newVBO)
-		{
-			VBO = newVBO;
-		}
-		void SetEBO(const GLuint& newEBO)
-		{
-			EBO = newEBO;
-		}
-		void SetVertices(const vector<AssimpVertex>& newVertices)
-		{
-			vertices = newVertices;
-		}
-		void SetIndices(const vector<unsigned int>& newIndices)
-		{
-			indices = newIndices;
-		}
-
-		const bool& IsEnabled() const
-		{
-			return isEnabled;
-		}
-		const MeshType& GetMeshType() const
-		{
-			return type;
-		}
-		const GLuint& GetVAO() const
-		{
-			return VAO;
-		}
-		const GLuint& GetVBO() const
-		{
-			return VBO;
-		}
-		const GLuint& GetEBO() const
-		{
-			return EBO;
-		}
-		const vector<AssimpVertex>& GetVertices() const
-		{
-			return vertices;
-		}
-		const vector<unsigned int>& GetIndices() const
-		{
-			return indices;
-		}
-	private:
-		bool isEnabled;
-		MeshType type;
-		GLuint VAO;
-		GLuint VBO;
-		GLuint EBO;
-		vector<AssimpVertex> vertices;
-		vector<unsigned int> indices;
-	};
-
-	class Material
-	{
-	public:
-		enum class TextureType
-		{
-			diffuse,
-			specular,
-			normal,
-			height,
-			misc_icon_blank,
-			misc_icon_move,
-			misc_icon_rotate,
-			misc_icon_scale
-		};
-
-		Material()
-		{
-		}
-
-		void AddTexture(const string& textureName, const unsigned int& textureID, const TextureType& textureType)
-		{
-			auto it = textures.find(textureType);
-			if (it != textures.end())
+			auto it = components.find(typeid(T));
+			if (it != components.end())
 			{
-				textures.erase(it);
+				return static_pointer_cast<T>(it->second);
 			}
-
-			textures[textureType][textureName] = textureID;
+			return nullptr;
+		}
+		template <typename T>
+		void RemoveComponent()
+		{
+			components.erase(typeid(T));
 		}
 
-		void AddShader(const string& vertShader, const string& fragShader, const Shader& newShader)
+		/*
+		* 
+		* PARENT
+		* 
+		*/
+		void SetParent(const shared_ptr<GameObject>& newParent)
 		{
-			shaderNames.push_back(vertShader);
-			shaderNames.push_back(fragShader);
-			shader = newShader;
+			parent = newParent;
 		}
-
-		const unsigned int& GetTextureID(const TextureType& type) const
+		shared_ptr<GameObject> GetParent() const
 		{
-			static unsigned int none = 0;
-
-			auto it = textures.find(type);
-			if (it != textures.end())
+			return parent.lock();
+		}
+		void RemoveParent()
+		{
+			if (auto currentParent = parent.lock())
 			{
-				const auto& textureMap = it->second;
-				if (!textureMap.empty())
-				{
-					return textureMap.begin()->second;
-				}
+				currentParent->RemoveChild(shared_from_this());
 			}
-			return none;
+			parent.reset();
 		}
-		const string& GetTextureName(const TextureType& type) const
-		{
-			static string none = "";
 
-			auto it = textures.find(type);
-			if (it != textures.end())
+		/*
+		* 
+		* CHILDREN
+		* 
+		*/
+		void AddChild(const shared_ptr<GameObject>& child)
+		{
+			children.push_back(child);
+			child->SetParent(shared_from_this());
+		}
+		const vector<shared_ptr<GameObject>>& GetChildren() const { return children; }
+		void RemoveChild(const shared_ptr<GameObject>& child)
+		{
+			children.erase(remove(children.begin(), children.end(), child), children.end());
+			if (child->GetParent().get() == this)
 			{
-				const auto& textureMap = it->second;
-				if (!textureMap.empty())
-				{
-					return textureMap.begin()->first;
-				}
+				child->SetParent(nullptr);
 			}
-			return none;
-		}
-		unsigned int GetTextureCount() const
-		{
-			unsigned int totalCount = 0;
-			for (const auto& pair : textures)
-			{
-				const auto& textureMap = pair.second;
-				totalCount += static_cast<unsigned int>(textureMap.size());
-			}
-			return totalCount;
-		}
-		bool TextureExists(const string& texturePath) const
-		{
-			for (const auto& pair : textures)
-			{
-				const auto& textureMap = pair.second;
-				for (const auto& texture : textureMap)
-				{
-					if (texture.first == texturePath)
-					{
-						return true;
-					}
-				}
-			}
-			return false;
 		}
 
-		const string& GetShaderName(unsigned int position) const
-		{
-			return shaderNames[position];
-		}
-		const Shader& GetShader() const
-		{
-			return shader;
-		}
-	private:
-		map<TextureType, map<string, unsigned int>> textures;
-		vector<string> shaderNames;
-		Shader shader;
-	};
-
-	class BasicShape_Variables
-	{
-	public:
-		BasicShape_Variables(const float& shininess) : shininess(shininess)
-		{
-		}
-
-		void SetShininess(const float& newShininess)
-		{
-			shininess = newShininess;
-		}
-
-		const float& GetShininess() const
-		{
-			return shininess;
-		}
-	private:
-		float shininess;
-	};
-
-	class PointLight_Variables
-	{
-	public:
-		PointLight_Variables(
-			const vec3& diffuse,
-			const float& intensity,
-			const float& distance) :
-			diffuse(diffuse),
-			intensity(intensity),
-			distance(distance)
-		{
-		}
-
-		void SetDiffuse(const vec3& newDiffuse)
-		{
-			diffuse = newDiffuse;
-		}
-		void SetIntensity(const float& newIntensity)
-		{
-			intensity = newIntensity;
-		}
-		void SetDistance(const float& newDistance)
-		{
-			distance = newDistance;
-		}
-
-		const vec3& GetDiffuse() const
-		{
-			return diffuse;
-		}
-		const float& GetIntensity() const
-		{
-			return intensity;
-		}
-		const float& GetDistance() const
-		{
-			return distance;
-		}
-	private:
-		vec3 diffuse;
-		float intensity;
-		float distance;
-	};
-
-	class SpotLight_Variables
-	{
-	public:
-		SpotLight_Variables(
-			const vec3& diffuse,
-			const float& intensity,
-			const float& distance,
-			const float& innerAngle,
-			const float& outerAngle) :
-			diffuse(diffuse),
-			intensity(intensity),
-			distance(distance),
-			innerAngle(innerAngle),
-			outerAngle(outerAngle)
-		{
-		}
-
-		void SetDiffuse(const vec3& newDiffuse)
-		{
-			diffuse = newDiffuse;
-		}
-		void SetIntensity(const float& newIntensity)
-		{
-			intensity = newIntensity;
-		}
-		void SetDistance(const float& newDistance)
-		{
-			distance = newDistance;
-		}
-		void SetInnerAngle(const float& newInnerAngle)
-		{
-			innerAngle = newInnerAngle;
-		}
-		void SetOuterAngle(const float& newOuterAngle)
-		{
-			outerAngle = newOuterAngle;
-		}
-
-		const vec3& GetDiffuse() const
-		{
-			return diffuse;
-		}
-		const float& GetIntensity() const
-		{
-			return intensity;
-		}
-		const float& GetDistance() const
-		{
-			return distance;
-		}
-		const float& GetInnerAngle() const
-		{
-			return innerAngle;
-		}
-		const float& GetOuterAngle() const
-		{
-			return outerAngle;
-		}
-	private:
-		vec3 diffuse;
-		float intensity;
-		float distance;
-		float innerAngle;
-		float outerAngle;
-	};
-
-	class Directional_light_Variables
-	{
-	public:
-		Directional_light_Variables(
-			const vec3& diffuse,
-			const float& intensity) :
-			diffuse(diffuse),
-			intensity(intensity)
-		{
-		}
-
-		void SetDiffuse(const vec3& newDiffuse)
-		{
-			diffuse = newDiffuse;
-		}
-		void SetIntensity(const float& newIntensity)
-		{
-			intensity = newIntensity;
-		}
-
-		const vec3& GetDiffuse() const
-		{
-			return diffuse;
-		}
-		const float& GetIntensity() const
-		{
-			return intensity;
-		}
-	private:
-		vec3 diffuse;
-		float intensity;
-	};
-
-	class GameObject
-	{
-	public:
-		static inline unsigned int nextID;
-
-		//basic gameobject
-		GameObject(
-			const bool& isInitialized,
-			const string& name,
-			const unsigned int& ID,
-			const bool& isEnabled,
-			const shared_ptr<Transform>& transform,
-			const shared_ptr<Mesh>& mesh,
-			const shared_ptr<Material>& material,
-			const shared_ptr<BasicShape_Variables>& basicShape) :
-			isInitialized(isInitialized),
-			name(name),
-			ID(ID),
-			isEnabled(isEnabled),
-			transform(transform),
-			mesh(mesh),
-			material(material),
-			basicShape(basicShape)
-		{
-		}
-
-		//point light
-		GameObject(
-			const bool& isInitialized,
-			const string& name,
-			const unsigned int& ID,
-			const bool& isEnabled,
-			const shared_ptr<Transform>& transform,
-			const shared_ptr<Mesh>& mesh,
-			const shared_ptr<Material>& material,
-			const shared_ptr<PointLight_Variables>& pointLight) :
-			isInitialized(isInitialized),
-			name(name),
-			ID(ID),
-			isEnabled(isEnabled),
-			transform(transform),
-			mesh(mesh),
-			material(material),
-			pointLight(pointLight)
-		{
-		}
-
-		//spotlight
-		GameObject(
-			const bool& isInitialized,
-			const string& name,
-			const unsigned int& ID,
-			const bool& isEnabled,
-			const shared_ptr<Transform>& transform,
-			const shared_ptr<Mesh>& mesh,
-			const shared_ptr<Material>& material,
-			const shared_ptr<SpotLight_Variables>& spotLight) :
-			isInitialized(isInitialized),
-			name(name),
-			ID(ID),
-			isEnabled(isEnabled),
-			transform(transform),
-			mesh(mesh),
-			material(material),
-			spotLight(spotLight)
-		{
-		}
-
-		//directional light
-		GameObject(
-			const bool& isInitialized,
-			const string& name,
-			const unsigned int& ID,
-			const bool& isEnabled,
-			const shared_ptr<Transform>& transform,
-			const shared_ptr<Mesh>& mesh,
-			const shared_ptr<Material>& material,
-			const shared_ptr<Directional_light_Variables>& directionalLight) :
-			isInitialized(isInitialized),
-			name(name),
-			ID(ID),
-			isEnabled(isEnabled),
-			transform(transform),
-			mesh(mesh),
-			material(material),
-			directionalLight(directionalLight)
-		{
-		}
-
-		//assimp model
-		GameObject(
-			const bool& isInitialized,
-			const string& name,
-			const unsigned int& ID,
-			const bool& isEnabled,
-			const shared_ptr<Transform>& transform,
-			const shared_ptr<Mesh>& mesh,
-			const vector<AssimpMesh>& assimpMeshes,
-			const shared_ptr<Material>& material) :
-			isInitialized(isInitialized),
-			name(name),
-			ID(ID),
-			isEnabled(isEnabled),
-			transform(transform),
-			mesh(mesh),
-			assimpMeshes(assimpMeshes),
-			material(material)
-		{
-		}
-
-		void Initialize() { isInitialized = true; }
+		/*
+		*
+		* BASIC PROPERTIES SHARED ACROSS ALL GAMEOBJECTS
+		*
+		*/
 		void SetName(const string& newName) { name = newName; }
-		void SetID(const unsigned int& newID) { ID = newID; }
-
-		void SetEnableState(const bool& newEnableState) { isEnabled = newEnableState; }
-
-		void SetTransform(const shared_ptr<Transform>& newTransform) { transform = newTransform; }
-		void SetMesh(const shared_ptr<Mesh>& newMesh) { mesh = newMesh; }
-		void AddAssimpMesh(const AssimpMesh& newMesh) { assimpMeshes.push_back(newMesh); }
-		void SetMaterial(const shared_ptr<Material>& newMaterial) { material = newMaterial; }
-		void SetBasicShape(const shared_ptr<BasicShape_Variables>& newBasicShape)
-		{
-			basicShape = newBasicShape;
-		}
-		void SetPointLight(const shared_ptr<PointLight_Variables>& newPointLight)
-		{
-			pointLight = newPointLight;
-		}
-		void SetSpotLight(const shared_ptr<SpotLight_Variables>& newSpotLight)
-		{
-			spotLight = newSpotLight;
-		}
-		void SetDirectionalLight(const shared_ptr<Directional_light_Variables>& newDirectionalLight)
-		{
-			directionalLight = newDirectionalLight;
-		}
-
-		void SetParent(const shared_ptr<GameObject>& newParent) { parent = newParent; }
-		void RemoveParent(const shared_ptr<GameObject>) { parent = nullptr; }
-		void AddChild(const shared_ptr<GameObject>& target, const shared_ptr<GameObject>& addedChild)
-		{
-			children.push_back(addedChild);
-			addedChild->SetParent(target);
-		}
-		void RemoveChild(const shared_ptr<GameObject>& removedChild)
-		{
-			children.erase(remove(children.begin(), children.end(), removedChild), children.end());
-			removedChild->RemoveParent(removedChild);
-		}
-		void SetParentBillboardHolder(const shared_ptr<GameObject>& newParentBillboardHolder)
-		{
-			parentBillboardHolder = newParentBillboardHolder;
-		}
-		void SetChildBillboard(const shared_ptr<GameObject>& newChildBillboard)
-		{
-			childBillboard = newChildBillboard;
-		}
+		const string& GetName() const { return name; }
 
 		void SetTxtFilePath(const string& newTxtFilePath) { txtFilePath = newTxtFilePath; }
-
-		const bool& IsInitialized() const { return isInitialized; }
-		const string& GetName() const { return name; }
-		const unsigned int& GetID() const { return ID; }
-
-		const bool& IsEnabled() const { return isEnabled; }
-
-		const shared_ptr<Transform>& GetTransform() const { return transform; }
-		const shared_ptr<Mesh>& GetMesh() const { return mesh; }
-		const vector<AssimpMesh>& GetAssimpMeshes() const { return assimpMeshes; }
-		const shared_ptr<Material>& GetMaterial() const { return material; }
-		const shared_ptr<BasicShape_Variables>& GetBasicShape() const { return basicShape; }
-		const shared_ptr<PointLight_Variables>& GetPointLight() const { return pointLight; }
-		const shared_ptr<SpotLight_Variables>& GetSpotLight() const { return spotLight; }
-		const shared_ptr<Directional_light_Variables>& GetDirectionalLight() const { return directionalLight; }
-		const shared_ptr<GameObject>& GetParent() const { return parent; }
-		const vector<shared_ptr<GameObject>>& GetChildren() const { return children; }
-		const shared_ptr<GameObject>& GetParentBillboardHolder() const { return parentBillboardHolder; }
-		const shared_ptr<GameObject>& GetChildBillboard() const { return childBillboard; }
 		const string& GetTxtFilePath() const { return txtFilePath; }
+
+		void SetEnableState(bool newEnableState) { isEnabled = newEnableState; }
+		bool IsEnabled() const { return isEnabled; }
+
+		void Initialize() { isInitialized = true; }
+		bool IsInitialized() const { return isInitialized; }
+
+		unsigned int GetID() const { return ID; }
+
+		const shared_ptr<TransformComponent>& GetTransform() const { return transform; }
+		void SetTransform(const shared_ptr<TransformComponent>& newTransform)
+		{
+			transform = newTransform;
+			AddComponent<TransformComponent>(newTransform);
+		}
+
+		weak_ptr<GameObject> parent;
+		vector<shared_ptr<GameObject>> children;
+
 	private:
-		bool isInitialized;
+		static inline unsigned int nextID = 0;
+
 		string name;
 		unsigned int ID;
-
+		bool isInitialized;
 		bool isEnabled;
-
-		shared_ptr<Transform> transform;
-		shared_ptr<Mesh> mesh;
-		vector<AssimpMesh> assimpMeshes;
-		shared_ptr<Material> material;
-		shared_ptr<BasicShape_Variables> basicShape;
-		shared_ptr<PointLight_Variables> pointLight;
-		shared_ptr<SpotLight_Variables> spotLight;
-		shared_ptr<Directional_light_Variables> directionalLight;
-		shared_ptr<GameObject> parent;
-		vector<shared_ptr<GameObject>> children;
-		shared_ptr<GameObject> parentBillboardHolder;
-		shared_ptr<GameObject> childBillboard;
 		string txtFilePath;
+
+		shared_ptr<TransformComponent> transform;
+
+		unordered_map<type_index, shared_ptr<Component>> components;
 	};
 
 	class GameObjectManager
