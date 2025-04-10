152530810e95d00e69687b4969fe9a79ff83cad8
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-04-11 02:05:23 +0300

    compilation improvements

--- Engine/build_linux_debug.sh
+++ Engine/build_linux_debug.sh
@@ -3,19 +3,15 @@
 # Set the root folder as the location of this script
 PROJECT_ROOT="$(dirname "$(readlink -f "$0")")"
 BUILD_DIR="$PROJECT_ROOT/build-debug"
-INSTALL_DIR="$PROJECT_ROOT/install-debug"
 
 # Record start time
 TIME_START=$(date +%T)
 
 cd "$PROJECT_ROOT"
 
-# Always start with a clean build and install directory
-echo "[INFO] Deleting build directory"
+# Always start with a clean build directory
 rm -rf "$BUILD_DIR"
-echo "[INFO] Deleting install directory"
-rm -rf "$INSTALL_DIR"
-mkdir -p "$BUILD_DIR" "$INSTALL_DIR"
+mkdir -p "$BUILD_DIR"
 
 cmake --preset=linux-debug
 if [ $? -ne 0 ]; then
@@ -35,24 +31,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-#Don't install executables
-#cmake --install build-debug
-#if [ $? -ne 0 ]; then
-#	echo "[ERROR] Install failed."
-#    if [[ "$1" == "pause" ]]; then
-#		read -r -p "Press enter to exit..."
-#	fi
-#    exit 1
-#fi
-
 # Record end time
 TIME_END=$(date +%T)
 
 # Success message
 echo "[SUCCESS] Project built and installed successfully."
 echo "---------------------------------------------"
-echo "Shared library: $INSTALL_DIR/lib/"
-echo "Include headers: $INSTALL_DIR/include"
+echo "Shared library: $BUILD_DIR/bin"
+echo "Static library: $BUILD_DIR/lib"
+echo "Include headers: $BUILD_DIR/include"
 echo "Build duration: $TIME_START - $TIME_END"
 echo "---------------------------------------------"
 
