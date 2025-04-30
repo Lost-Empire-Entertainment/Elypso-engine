fffe01e0f5bb1cc070d055247c4368f1f0cc0287
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-05-01 02:04:32 +0300

    up to date binaries + better build systems

--- build_all_linux.sh
+++ build_all_linux.sh
@@ -1,32 +1,63 @@
 #!/bin/bash
 
-PROJECT_ROOT="$(dirname "$(readlink -f "$0")")"
-cd "$PROJECT_ROOT"
+# Set root folder
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set shell script paths
+ENGINE_RELEASE_SH="${ROOT_DIR}/Engine/build_linux_release.sh"
+ENGINE_DEBUG_SH="${ROOT_DIR}/Engine/build_linux_debug.sh"
+ENGINE_LIB_RELEASE_SH="${ROOT_DIR}/Engine library/build_linux_release.sh"
+ENGINE_LIB_DEBUG_SH="${ROOT_DIR}/Engine library/build_linux_debug.sh"
+COPY_SH="${ROOT_DIR}/_external_shared/copy_linux.sh"
+
+# Confirm if shell script paths are valid
+if [[ ! -f "$COPY_SH" ]]; then
+    echo "[ERROR] Copy shell script does not exist: $COPY_SH"
+    read -r -p "Press enter to exit..."
+    exit 1
+fi
+if [[ ! -f "$RELEASE_SH" ]]; then
+    echo "[ERROR] Release shell script does not exist: $RELEASE_SH"
+    read -r -p "Press enter to exit..."
+    exit 1
+fi
+if [[ ! -f "$DEBUG_SH" ]]; then
+    echo "[ERROR] Debug shell script does not exist: $DEBUG_SH"
+    read -r -p "Press enter to exit..."
+    exit 1
+fi
+
+echo "====================================="
+echo "[INFO] Copying external binaries..."
+echo "====================================="
+echo ""
+
+# Copy external shared
+if ! bash "${COPY_SH}"; then
+    echo "[FATAL] Copy failed!"
+    read -r -p "Press enter to exit..."
+    exit 1
+fi
 
 echo "====================================="
 echo "[INFO] Building engine in Release mode..."
 echo "====================================="
 echo ""
 
-cd "$PROJECT_ROOT/Engine"
-
-bash "./build_linux_release.sh"
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Release build failed."
-    read -p "Press enter to continue..."
+if ! bash "${ENGINE_RELEASE_SH}"; then
+    echo "[FATAL] Engine Release build failed!"
+    read -r -p "Press enter to exit..."
     exit 1
 fi
 
-echo ""
 echo "====================================="
-echo "[INFO] Building hub in Debug mode..."
+echo "[INFO] Building engine in Debug mode..."
 echo "====================================="
 echo ""
 
-bash "./build_linux_debug.sh"
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Debug build failed."
-    read -p "Press enter to continue..."
+if ! bash "${ENGINE_DEBUG_SH}"; then
+    echo "[FATAL] Engine Debug build failed!"
+    read -r -p "Press enter to exit..."
     exit 1
 fi
 
@@ -35,25 +66,20 @@ echo "[INFO] Building engine library in Release mode..."
 echo "====================================="
 echo ""
 
-cd "$PROJECT_ROOT/Engine library"
-
-bash "./build_linux_release.sh"
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Release build failed."
-    read -p "Press enter to continue..."
+if ! bash "${ENGINE_LIB_RELEASE_SH}"; then
+    echo "[FATAL] Engine Library Release build failed!"
+    read -r -p "Press enter to exit..."
     exit 1
 fi
 
-echo ""
 echo "====================================="
 echo "[INFO] Building engine library in Debug mode..."
 echo "====================================="
 echo ""
 
-bash "./build_linux_debug.sh"
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Debug build failed."
-    read -p "Press enter to continue..."
+if ! bash "${ENGINE_LIB_DEBUG_SH}"; then
+    echo "[FATAL] Engine Library Debug build failed!"
+    read -r -p "Press enter to exit..."
     exit 1
 fi
 
