36defb259cf96ce47779b265aa85c57aa0f5530d
greeenlaser <sanderveski@gmail.com>
2026-03-27 18:50:36 +0200

    move external shared to other repo

--- _external_shared/KalaGraphics/include/objects/kg_camera.hpp
+++
@@ -1,4 +0,0 @@
-//Copyright(C) 2026 Lost Empire Entertainment
-//This program comes with ABSOLUTELY NO WARRANTY.
-//This is free software, and you are welcome to redistribute it under certain conditions.
-//Read LICENSE.md for more information.
